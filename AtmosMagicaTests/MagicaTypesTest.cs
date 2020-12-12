caca69a3d7624c769b15003357b5bee0fe463e5d
Pl4yingNight <pl4yingnight@gmail.com>
2020-12-12 23:36:28 +0100

    added unit test project

---
+++ AtmosMagicaTests/MagicaTypesTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtmosMagica.Environment.Types;
+
+namespace AtmosMagicaTests
+{
+    [TestClass]
+    public class MagicaTypesTest
+    {
+        [TestMethod]
+        public void GetTypeTest()
+        {
+            new ElementalMagica();
+
+        }
+    }
+}
---
+++ AtmosMagicaTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AtmosMagicaTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtmosMagicaTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c79d9d4b-3a1b-4d4c-a7be-bde107e254e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
