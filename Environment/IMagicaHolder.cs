5a7395ff963a1b4d422f225cc3c67204c2b8e92e
Pl4yingNight <pl4yingnight@gmail.com>
2020-12-13 17:32:15 +0100

    latest ideas

---
+++ Environment/IMagicaHolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmosMagica.Environment
+{
+    public interface IMagicaHolder<M> where M : Magica
+    {
+        M GetMagica();
+    }
+}
